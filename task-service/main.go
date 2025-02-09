fe80de7d4734a1c940c7bdd6d17a19f4f56353ca
Yura Sahanovskyi <yuriisahanovskyi@gmail.com>
2025-02-09 14:54:30 +0200

    Web server

--- task-service/main.go
+++ task-service/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Hello from task service"))
+
+}
 
 func main() {
 	fmt.Println("Hello from task service")
+	http.HandleFunc("GET /", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
